9f9d495c56b480f373a2b4d8fa4940ac2287b0cf
null <null>
2026-08-20 15:13:08 +0900

    make add node

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,26 @@ function App(){
     {id: "企業分析"},
     {id: "ゲーミフィケーション"},
   ])
+
+  // リンク一覧
+  const [links, setLinks] = useState([
+    { source: 'ナノコネ', target: '業界研究' },
+    { source: 'ナノコネ', target: 'ゲーミフィケーション' },
+    { source: 'ナノコネ', target: '企業分析' }
+  ])
+
   //最初は空文字
   const [input, setInput] = useState("")
 
+  // 接続先
+  // 最初は「ナノコネ」
+  const [target, setTarget] = useState('ナノコネ')
+
+  // グラフに入れるデータ
   const graphData = {
     nodes: nodes,
-    // link はとりあえず、自分で定義
-    links: [
-      {source: "ナノコネ", target: "業界研究"},
-      {source: "ナノコネ", target: "ゲーミフィケーション"},
-    ],
+    links: links,
   }
-
 
   // ボタン押されたとき実行される関数（アロー関数）
   const addNode = () => {
@@ -35,12 +43,23 @@ function App(){
       {id: input},
     ])
 
+    // 現在のリンクに追加
+    setLinks([
+      ...links,
+      //既存のlinksに追加する内容
+      {
+        source: target, // from
+        target: input, // to
+      },
+    ])
+
     //ノード追加後、入力欄を空に
     setInput("");
   }
   //ここからReact
   return(
     <div>
+      {/*入力設定 */}
       <input 
       type="text" 
       value={input}
@@ -50,6 +69,22 @@ function App(){
       //入力欄の説明
       placeholder="追加する情報を入力"
       />
+
+      {/*接続先 
+      複数の候補から1つ選ぶプルダウン
+      */}
+      <select 
+      value={target}
+      //選択肢が変更されたときの処理
+      onChange={(e) => setTarget(e.target.value)}
+      >   
+          {/*配列をループして一覧に map */}
+         {nodes.map((node) => (
+          <option key={node.id} value={node.id}>
+            {node.id}
+          </option>
+        ))}
+      </select>
 
       {/*クリックするとaddNode */}
       <button onClick={addNode}>
