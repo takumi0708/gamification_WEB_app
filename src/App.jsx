eae67c2747aea0e06bb092211e2e7df8a743cd03
null <null>
2026-08-20 14:35:33 +0900

    add input node

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,77 @@
-import ForceGraph2D from 'react-force-graph-2d'
+import { useState } from "react"
+import ForceGraph2D from "react-force-graph-2d"
 
-// App 関数宣言
 function App(){
-  // グラフデータ保存する変数
-  // graphData オブジェクトのvalueに配列を入れてる
-  // その配列の中身にオブジェクトを入れてる
+  // nodes      : 現在のノード一覧
+  // setNodes   : nodesを書き換える関数
+  const [nodes, setNodes] = useState([
+    {id: "ナノコネ"},
+    {id: "業界研究"},
+    {id: "企業分析"},
+    {id: "ゲーミフィケーション"},
+  ])
+  //最初は空文字
+  const [input, setInput] = useState("")
+
   const graphData = {
-    nodes: [
-      { id: 'ナノコネ' },
-      { id: 'ゲーム業界' },
-      { id: 'ゲーミフィケーション' },
-      { id: 'エンジニア' },
-      { id: '企画職' },
-    ],
+    nodes: nodes,
+    // link はとりあえず、自分で定義
     links: [
-      { source: 'ナノコネ', target: 'ゲーム業界' },
-      { source: 'ナノコネ', target: 'ゲーミフィケーション' },
-      { source: 'ナノコネ', target: 'エンジニア' },
-      { source: 'ナノコネ', target: '企画職' },
+      {source: "ナノコネ", target: "業界研究"},
+      {source: "ナノコネ", target: "ゲーミフィケーション"},
     ],
   }
-  // JSX を書けるReactの記法
-  /*
-  画面縦横いっぱい
-  ForceGraph2DはReact 部品（ここに２Dグラフ表示）
-  node label はid使う指定
-  */
+
+
+  // ボタン押されたとき実行される関数（アロー関数）
+  const addNode = () => {
+    // 何もないとき用
+    if(input == " ") return
+
+    // 現在のノードに追加
+    setNodes([
+      // 今あるnodes をすべて展開
+      ...nodes,
+      {id: input},
+    ])
+
+    //ノード追加後、入力欄を空に
+    setInput("");
+  }
+  //ここからReact
   return(
-    <div style={{ width: '100vw', height: '100vh' }}>
+    <div>
+      <input 
+      type="text" 
+      value={input}
+      // 文字が入力されるたびにinput Stateを更新
+      onChange={(e) => setInput(e.target.value)}
+
+      //入力欄の説明
+      placeholder="追加する情報を入力"
+      />
+
+      {/*クリックするとaddNode */}
+      <button onClick={addNode}>
+        追加
+      </button>
+
+      {/*グラフ表示 */}
       <ForceGraph2D
+
+        // nodesとlinksを渡す
         graphData={graphData}
-        nodeLavel="id"
-        linkColor={() => "white"}
-        linkWidth={2}
+
+        // ノードにマウスを合わせたときidを表示
+        nodeLabel="id"
+
+        // linkの色を白にする
+        linkColor={() => 'white'}
+
+        // linkの太さ
+        linkWidth={1}
       />
     </div>
   )
 }
-//このファイルの機能を他ファイルから使えるように
 export default App
